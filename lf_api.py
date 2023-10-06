11431b7d1d46da1762aa2cb0c751dcfcb3cf6848
Paul Häussler <paulhaeussler@mailbox.org>
2023-10-06 16:57:02 +0200

    fix

--- lf_api.py
+++ lf_api.py
@@ -14,7 +14,7 @@ async def root():
     return {"message": "This is the backend to the Lovoofarmer Extension"}
 
 
-@app.put("v1/profile/{profile_id}")
+@app.put("/v1/profile/{profile_id}")
 async def profile(profile_id: str, profile: Profile):
     print(profile_id)
     print(profile)
